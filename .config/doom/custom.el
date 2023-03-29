d83dc63c9da0f82b6ac68f72b9a308c1464eeb4d
Sebastian Slangerup Møller <sebastian.slangerup1@gmail.com>
2023-03-29 09:18:45 +0200

    Added school files to org agenda

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default)))
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(org-agenda-files
+   '("~/org/h3/agenda.org" "/Users/sebastian/org/agenda.org" "/Users/sebastian/org/the-php-dev.org"))
+ '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
