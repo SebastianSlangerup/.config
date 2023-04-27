95b9842378532c37f5c4a1d26f5ab4632b75fcff
Sebastian Slangerup Møller <sebastian.slangerup1@gmail.com>
2023-04-27 08:36:21 +0200

    Doom Update

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -56,7 +56,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-one)
 
 ;; Change theme depending on time of day
 ;;(setq circadian-themes '(("7:00" . doom-one-light)
--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(org-agenda-files
    '("~/org/h3/agenda.org" "/Users/sebastian/org/agenda.org" "/Users/sebastian/org/the-php-dev.org"))
  '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)")))
